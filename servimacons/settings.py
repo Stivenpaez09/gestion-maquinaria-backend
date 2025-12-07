ab9c7ffb15f28f257566d8d7733a4631ab389cdf
Stivenpaez09 <stivenpaez09@gmail.com>
2025-12-06 23:41:03 -0500

    build: agregar archivos de configuracion para despliegue

--- servimacons/settings.py
+++ servimacons/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gestion-maquinaria-backend.onrender.com']
+ALLOWED_HOSTS = ['localhost:4200', '127.0.0.1', 'gestion-maquinaria-backend.onrender.com']
 
 
 # Application definition
